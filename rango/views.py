df1a2aa994bec6773b0ca89bf64c3f62ca6b86a6
Conor Begley <2236693B@student.gla.ac.uk>
2018-01-21 16:31:56 +0000

    Chapter 3 revised

--- rango/views.py
+++ rango/views.py
@@ -5,3 +5,8 @@ def index(request):
     context_dict = { 'boldmessage': "Crunchy, creamy. cookie, candy, cupcake!"}
 
     return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+    context_dict = { 'boldmessage': "Rango says here is the about page"}
+
+    return render(request, 'rango/index.html', context=context_dict)
