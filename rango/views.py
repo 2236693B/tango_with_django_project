83fa319a381235acc3257d417477b68b338b36e4
Conor Begley <2236693B@student.gla.ac.uk>
2018-01-22 17:16:05 +0000

    Chapter Four final

--- rango/views.py
+++ rango/views.py
@@ -7,6 +7,6 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    context_dict = { 'boldmessage': "Rango says here is the about page"}
+    context_dict = { 'boldmessage': "This tutorial has been put togther by: Conor"}
 
-    return HttpResponse("Rango says here is the about pages <a href='#'>abc</a>")
+    return return render(request, 'rango/about.html', context=context_dict)
